a37bf1d3866af64272422bed1b62ab7c49bd95c2
jaime Guerrero <jaimegordon20@outlook.com>
2020-02-19 19:16:21 -0500

    help2

--- src/CAI1.java
+++ src/CAI1.java
@@ -9,21 +9,7 @@ public class CAI1 {
 	static int number1, number2;
 	static Scanner input = new Scanner(System.in);
 	
-	/*The program shall generate random numbers with a SecureRandom object
-	The program shall ask the student to solve a multiplication problem
-	A multiplication problem shall contain two numbers sampled from a uniform random distribution in the range of 0 to 9 (inclusive)
-	The program shall display the message "Very good!" if the student provides a correct response
-	The program shall display the message "No. Please try again." if the student provides an incorrect response
-	The program shall terminate when a correct response is provided by the student
-	The program shall continue to ask the student to solve the original multiplication problem until the student provides the correct answer
-	Create a method called "quiz" that contains the program logic
-	Create a method called "askQuestion" that prints the problem to the screen
-	Create a method called "readResponse" that reads the answer from the student
-	Create a method called "isAsnwerCorrect" that checks to see if the student's answer matches the correct answer to the problem
-	Create a method called "displayCorrectResponse" that prints out the response when a student enters a correct answer
-	Create a method called "displayInorrectResponse" that prints out the response when a student enters an incorrect answer
-	Create a main method that runs your program by calling the "quiz" method
-	 */
+	
 	
 	public void quiz() 
 	{
